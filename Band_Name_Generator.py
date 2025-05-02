85913f3c71b42791381e225a758e33043e028fa1
Noah Raphael <noahraphael@Noahs-Magical-Mac.local>
2025-05-01 22:23:56 -0400

    Day 1 Project, Band name generator

---
+++ Band_Name_Generator.py
@@ -0,0 +1,8 @@
+print("Welcome to the band name generator!")
+
+city = input("Please input the city you grew up in:\n")
+pet = input("Please input your pets name:\n")
+
+band_name = city + " " + pet
+
+print(f'Your new band name is... {band_name}, congratulations!!!')
